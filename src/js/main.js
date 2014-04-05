c2585a2b5fa51e77a9f13ed44d9a0e47a30ae894
Eric Conrad <eric.jeffrey.conrad@gmail.com>
2014-04-04 23:12:20 -0400

    added gentona and quadon reorganized and relinked font awesome added color variables converted sass files to scss created (really) rough js to vertically and horizontally center an object in it's parent. Should center and resize the font on page load and on window resize.

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,70 @@
 var log = require("./lib/log"),
     $ = require("./vendor/jquery");
 
+function hovercar (el) {
+  var $el = $(el);
+  var width = $el.outerWidth( true );
+  function newWidth ($el) { width = $el.outerWidth( true ); }
+  var height = $el.outerHeight( true );
+  function newHeight ($el) { height = $el.outerHeight( true ); }
+  var pWidth = $el.parent().outerWidth( true );
+  function newPWidth ($el) { pWidth = $el.parent().outerWidth( true ); }
+  var pHeight = $el.parent().outerHeight( true );
+  function newPHeight ($el) { pHeight = $el.parent().outerHeight( true ); }
 
-$(function () {
+  // console.log($el);
+  //
+  // console.log("element = " + width + "px" + " x " + height + "px");
+  // console.log("parent = " + pWidth + "px" + " x " + pHeight + "px");
 
-  log("The document is ready and we have some js running, way to go. Big accomplishment. We're all pulling for you.");
+  var fontSize = Number($el.css("font-size").replace(/[^\d.-]/g, ''));
+  var newFontSize = fontSize;
 
+  function center ($el) {
+    newWidth($el);
+    newHeight($el);
+    newPWidth($el);
+    newPHeight($el);
+    $el.css({
+      "top": ((pHeight - height) / 2) + "px",
+      "left": ((pWidth - width) / 2) + "px",
+    });
+    console.log("center() was called");
+  }
+
+  function fontSizer (scale, $el) {
+    newFontSize = newFontSize * scale;
+    $el.css({
+      "font-size": (newFontSize / 10) + "rem",
+    });
+  }
+
+  function fontSizeChecker ($el) {
+    if ( width >= (pWidth * 0.9)) {
+      fontSizer(0.9, $el);
+      console.log("after css change" + fontSize + ", " + newFontSize);
+      center($el);
+    } else if ( width < (pWidth * 0.8) && newFontSize < fontSize ) {
+      fontSizer(1.11111111, $el);
+      if ( newFontSize > fontSize ) {
+        newFontSize = fontSize;
+        console.log("after css change" + fontSize + ", " + newFontSize);
+      }
+      console.log("after css change" + fontSize + ", " + newFontSize);
+      center($el);
+    }
+  }
+
+  fontSizeChecker($el);
+  center($el);
+
+  $(window).resize( function() {
+    fontSizeChecker($el);
+    center($el);
+  });
+
+}
+
+$(window).load( function() {
+  hovercar($(".hovercar"));
 });
