426dc80c9ff5dfedbf78ea8ea73b1b86fcb3de97
Eric Conrad <eric.jeffrey.conrad@gmail.com>
2014-04-24 11:13:20 -0400

    Shit tonne of work done here
    Basically re-writing the whole thing.
    • Added specific dimension checking for the element based on media type.
    • Added ability to have multiple hover cars and license plates in the
    same document;
    • Added a check to make sure the photo/image would always fill the
    screen (width/height auto switch).
    • Still needs a lot of refining.

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,25 @@
 var log = require("./lib/log"),
     $ = require("./vendor/jquery");
 
-function hovercar (el) {
-  var $el = $(el);
-  var width = $el.outerWidth( true );
-  var height = $el.outerHeight( true );
-  var pWidth = $el.parent().outerWidth( true );
-  var pHeight = $el.parent().outerHeight( true );
-  var dimensions = getDimensions($el);
-  // console.log($el);
-  //
-  // console.log("element = " + width + "px" + " x " + height + "px");
-  // console.log("parent = " + pWidth + "px" + " x " + pHeight + "px");
-
-  var fontSize = Number($el.css("font-size").replace(/[^\d.-]/g, ''));
-  var newFontSize = fontSize;
-
-  function newWidth ($el) { width = $el.outerWidth( true ); }
-  function newHeight ($el) { height = $el.outerHeight( true ); }
-  function newPWidth ($el) { pWidth = $el.parent().outerWidth( true ); }
-  function newPHeight ($el) { pHeight = $el.parent().outerHeight( true ); }
+function hovercar (cars, plates) {
+  var $cars = $(cars);
+  var $plates = $(plates);
+  if ($cars.length === 0 && $plates.length === 0) { return; }
 
   function getDimensions ($el) {
     var dimensions = {};
     dimensions.width = $el.outerWidth( true );
     dimensions.height = $el.outerHeight( true );
+    if ( $el.context.nodeName === "VIDEO" ) {
+      dimensions.nativeWidth = $el.context.videoWidth;
+      dimensions.nativeHeight = $el.context.videoHeight;
+    } else if ( $el.context.nodeName === "IMG" ) {
+      dimensions.nativeWidth = $el.context.naturalWidth;
+      dimensions.nativeHeight = $el.context.naturalHeight;
+    } else {
+      dimensions.nativeWidth = 0;
+      dimensions.nativeHeight = 0;
+    }
     dimensions.parent = {};
     dimensions.parent.width = $el.parent().outerWidth( true );
     dimensions.parent.height = $el.parent().outerHeight( true );
@@ -33,66 +28,104 @@ function hovercar (el) {
   }
 
   function center ($el) {
-    // I feel like there should be an easier way to set the height variable and
-    // then refresh it on resize.
-    // newWidth($el);
-    // newHeight($el);
-    // newPWidth($el);
-    // newPHeight($el);
-
-    // this only works if the parent element is set postion: relative; and if
-    // the target element is set position: absolute;. it'd be nice to do a check
-    // of these two to see if they already have the position attribute and then
-    // tweak the position based on that and/or add the position to each.
-    getDimensions($el);
+    var dims = getDimensions($el);
 
     $el.css({
-      "top": ((dimensions.parent.height - dimensions.height) / 2) + "px",
-      "left": ((dimensions.parent.width - dimensions.width) / 2) + "px",
-    });
-    console.log("//////////RESIZE//////////");
-    console.log("$el: " + dimensions.width + " x " + dimensions.height);
-    console.log("parent: " + dimensions.parent.width + " x " + dimensions.parent.height);
-    console.log("window: " + window.innerWidth + " x " + window.innerHeight);
-  }
-
-  function fontSizer (scale, $el) {
-    newFontSize = newFontSize * scale;
-    $el.css({
-      "font-size": (newFontSize / 10) + "rem",
+      "top": ((dims.parent.height - dims.height) / 2) + "px",
+      "left": ((dims.parent.width - dims.width) / 2) + "px",
     });
   }
 
-  function fontSizeChecker ($el) {
-    // this whole if check is a little dicey I think. Not sure if there's a
-    // better way or if I should pass in the scaling factors when the function
-    // is called. Should the scaling up and down be separate functions?
-    if ( dimensions.width >= (dimensions.parent.width * 0.9)) {
-      fontSizer(0.9, $el);
-      // console.log("after css change" + fontSize + ", " + newFontSize);
-      center($el);
-    } else if ( dimensions.width < (dimensions.parent.width * 0.8) && newFontSize < fontSize ) {
-      fontSizer(1.11111111, $el);
-      // is there a better way to do this rather than nesting ifs?
-      if ( newFontSize > fontSize ) {
-        newFontSize = fontSize;
-        // console.log("after css change" + fontSize + ", " + newFontSize);
-      }
-      // console.log("after css change" + fontSize + ", " + newFontSize);
-      center($el);
+  function liftOff ($el) {
+    accidentWarning($el);
+    center($el);
+  }
+
+  function accidentWarning (car) {
+    var dims = getDimensions(car);
+    var carRatio = dims.nativeWidth / dims.nativeHeight;
+    var parkingSpace = dims.parent.width / dims.parent.height;
+
+    if ( carRatio >= parkingSpace ) {
+      car.css({
+        "width": "auto",
+        "height": dims.parent.height + "px",
+      });
+    } else {
+      car.css({
+        "width": dims.parent.width + "px",
+        "height": "auto",
+      });
     }
   }
 
-  fontSizeChecker($el);
-  center($el);
+  function licensePlate (plate) {
+    var fontSize = Number(plate.css("font-size").replace(/[^\d.-]/g, ''));
+    var newFontSize = fontSize;
+    if ($plates.length === 0) { return; }
 
-  $(window).resize( function() {
-    fontSizeChecker($el);
-    center($el);
+    function fontResize (scale, plate) {
+      newFontSize = newFontSize * scale;
+      plate.css({
+        "font-size": (newFontSize / 10) + "rem",
+      });
+    }
+
+    function checkFontSize (plate) {
+      var dims = getDimensions(plate);
+      if ( dims.width >= (dims.parent.width * 0.9) ) {
+        fontResize(0.9, plate);
+        checkFontSize(plate);
+      } else if ( dims.width < (dims.parent.width * 0.8) && newFontSize < fontSize ) {
+        fontResize(1.11111111, plate);
+      } else if ( dims.width < (dims.parent.width * 0.8) && newFontSize >= fontSize ) {
+        newFontSize = fontSize;
+      } else {
+        return;
+      }
+    }
+
+    $plates.each( function () {
+      var plate = $(this);
+      checkFontSize(plate);
+      center(plate);
+
+      $(window).resize( function () {
+        checkFontSize(plate);
+        center(plate);
+      });
+    });
+  }
+
+  $cars.each( function () {
+    var car = $(this);
+    var dims = getDimensions(car);
+    var i = 0;
+    function preFlightCheck ($el, max) {
+      setTimeout(function () {
+        dims = getDimensions($el);
+        if ( dims.nativeHeight > 0 ) { liftOff($el); }
+        else if (i < max) {
+          i = i + 1;
+          preFlightCheck($el, max);
+        }
+      }, 50);
+    }
+    if ( Number(dims.nativeHeight) >= 0 ) {
+      preFlightCheck(car, 10);
+    }
+
+    $(window).resize( function () {
+      liftOff(car);
+    });
   });
 
+  $plates.each( function () {
+    var plate = $(this);
+    licensePlate(plate);
+  });
 }
 
-$(window).load( function() {
-  hovercar($(".hovercar"));
+$(window).load( function () {
+  hovercar($(".hovercar"), $(".license-plate"));
 });
